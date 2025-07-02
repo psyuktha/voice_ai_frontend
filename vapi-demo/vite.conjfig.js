d7224f42245c5ee007b1f33c35a3e033f2138823
psyuktha <1ds22is191@dsce.edu.in>
2025-07-02 18:19:32 +0530

    .

--- vapi-demo/vite.conjfig.js
+++ vapi-demo/vite.conjfig.js
@@ -5,10 +5,11 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
-    allowedHosts: ['voice-ai-frontend-1vbf.onrender.com'] 
+    allowedHosts: ['voice-ai-frontend-1vbf.onrender.com']
   },
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    allowedHosts: ['voice-ai-frontend-1vbf.onrender.com'] 
   }
 })
